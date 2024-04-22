2678a5e03efebde2b7fed2f137cff3ee171aa0bf
anetczuk <anetczuk@o2.pl>
2024-04-23 00:54:16 +0200

    Add tools

---
+++ examples/generate-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+## generate small images
+# $SCRIPT_DIR/generate_small.sh
---
+++ process-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+$SCRIPT_DIR/doc/generate-doc.sh
+
+$SCRIPT_DIR/examples/generate-all.sh
+
+$SCRIPT_DIR/tools/mdpreproc.py $SCRIPT_DIR/README.md
+
+$SCRIPT_DIR/tools/checkall.sh
+
+
+echo "processing completed"
